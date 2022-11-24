d5e6d7bdfba557448517d69ae8473ece743a0619
SsstassS38 <Stasonline26@gmail.com>
2022-11-24 11:45:07 +0300

    add Example54

--- HomeworkCsharp/SeminarsHomeworkCsharp/Homework8/Example54/obj/Debug/net6.0/Example54.AssemblyInfo.cs
+++ HomeworkCsharp/SeminarsHomeworkCsharp/Homework8/Example54/obj/Debug/net6.0/Example54.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Example58")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Example54")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Example58")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Example58")]
+[assembly: System.Reflection.AssemblyProductAttribute("Example54")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Example54")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
